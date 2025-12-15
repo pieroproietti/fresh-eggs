4b8e2e1adff8fec32ced995fb42451d8d198e50e
Piero Proietti <piero.proietti@gmail.com>
2025-12-15 15:23:17 +0100

    v25.12.15

--- fresh-eggs.sh
+++ fresh-eggs.sh
@@ -8,8 +8,8 @@
 # ==============================================================================
 
 # --- Variabili Globali ---
-LAST_VERSION="25.12.8"
-LAST_RELEASE="2"
+LAST_VERSION="25.12.15"
+LAST_RELEASE="1"
 URL_BASE="https://penguins-eggs.net/basket/packages"
 
 source ./ensure-node22.sh
