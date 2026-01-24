ceac333301f0ef1683b4738b2c9289a217e3c25c
Piero Proietti <piero.proietti@gmail.com>
2026-01-24 19:06:36 +0100

    v26.1.24

--- fresh-eggs.sh
+++ fresh-eggs.sh
@@ -8,7 +8,7 @@
 # ==============================================================================
 
 # --- Variabili Globali ---
-LAST_VERSION="26.1.21"
+LAST_VERSION="26.1.24"
 LAST_RELEASE="1"
 URL_BASE="https://penguins-eggs.net/basket/packages"
 
